50e904ee7adcabea3231d8cb18b56c93b2161d5c
Christian Dallago <code@dallago.us>
2019-03-26 16:35:19 +0100

    no cache for heroku

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
-import os
 import re
-import pandas as pd
 from extractor import extract_locations_from_URL
 from flask import Flask, request, abort
 from flask_cors import cross_origin
 
 app = Flask(__name__)
-
-# Check if cache file exists. If not, create it:
-if os.path.isfile("cache"):
-    print("Cache exists: loading...")
-    cache = pd.read_csv("cache", sep="\t")
-else:
-    print("Cache doesn't exists: creating...")
-    cache = extract_locations_from_URL()
-    cache.to_csv("cache", sep='\t', index=False)
-
-print("Cache loaded.")
 
 UNIPROT_AC_REGEX = re.compile("[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}")
 
@@ -28,18 +15,13 @@ def get_location():
 
     if query:
         if UNIPROT_AC_REGEX.match(query):
-            cache_results = cache[cache['accession'] == query]
-
-            if cache_results.empty:
-                results = extract_locations_from_URL("https://www.uniprot.org/uniprot/{}.xml".format(query))
-                return results.to_json(orient="records")
-            else:
-                return cache_results.to_json(orient="records")
+            results = extract_locations_from_URL("https://www.uniprot.org/uniprot/{}.xml".format(query))
+            return results.to_json(orient="records")
         else:
             abort(400)
 
     else:
-        return cache.to_json(orient="records")
+        abort(404)
 
 
 if __name__ == '__main__':
