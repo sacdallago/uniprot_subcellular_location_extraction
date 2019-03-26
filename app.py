1031d1fc454be0b4f05314ac2c25669cd41e245f
Christian Dallago <code@dallago.us>
2019-03-26 15:43:18 +0100

    allow cross origin

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import re
 import pandas as pd
 from extractor import extract_locations_from_URL
 from flask import Flask, request, abort
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
@@ -19,7 +20,9 @@ print("Cache loaded.")
 
 UNIPROT_AC_REGEX = re.compile("[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}")
 
+
 @app.route('/')
+@cross_origin()
 def get_location():
     query = request.args.get('q')
 
